ef45f3a6786949950a83935fcec870229a0bc01a
Your Name <shlokjagtap123@gmail.com>
2024-06-06 14:43:43 +0530

    testing to be done

---
+++ next/src/app/api/user/me/route.ts
@@ -0,0 +1,39 @@
+import { connect } from "@/dbConfig/dbConfig"
+import User from '@/models/userModel'
+import { NextRequest, NextResponse } from "next/server"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import { getDataFromToken } from "@/helpers/getdatafromtoken"
+
+connect()
+
+export async function POST(request: NextRequest) { 
+        
+
+
+
+
+
+
+
+    try {
+    
+
+            const userId = await getDataFromToken(request)
+    const user = User.findOne({ _id: userId }).select("-password")
+    
+
+    return NextResponse.json({
+        message: "User found",
+        data:user
+    })
+
+
+    } catch (error: any) {
+        return NextResponse.json({ error: "User not found" },
+            {status:500}
+        )
+    }
+    
+    
+}
---
+++ next/src/helpers/getdatafromtoken.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken"
+
+
+
+export const getDataFromToken = (request: NextRequest)=>{
+    
+    try {
+        const token = request.cookies.get("token")?.value || "";
+        const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!)
+        
+
+        return decodedToken.id
+    } catch (erro: any) {
+        return NextResponse.json("ERROR")
+}
+
+}
+
